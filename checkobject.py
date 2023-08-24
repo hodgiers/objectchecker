9a7ed26c8f187ffdd65c2f135c95a3471e6d2144
Rashan Hodge <rhodge@macminiPro.local>
2023-08-24 19:46:12 -0400

    Initial Commit

---
+++ checkobject.py
@@ -0,0 +1,71 @@
+from netaddr import IPNetwork, cidr_merge
+import requests
+import xml.etree.ElementTree as ET
+import json
+import urllib3
+import re
+import os
+import csv
+from dotenv import load_dotenv
+load_dotenv()
+
+api_key = os.getenv('PAN_API_KEY')
+device_group = os.getenv('DEVICE_GROUP')
+fw_host = os.getenv("FW_HOST")
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+print(f"Device Group: {device_group}")
+print(f"Hostname: {fw_host}")
+
+
+
+# Read addresses from CSV into a dictionary
+addresses_from_csv = {}
+with open('shared_objects.csv', 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        addresses_from_csv[row['Name']] = row['Address']
+
+
+# REST API URL
+url_prerule = f"https://{fw_host}/restapi/v10.2/Policies/SecurityPreRules?location=device-group&device-group={device_group}"
+url_postrule = f"https://{fw_host}/restapi/v10.2/Policies/SecurityPostRules?location=device-group&device-group={device_group}"
+
+# Headers
+headers = {
+    'X-PAN-KEY': api_key,
+}
+
+# Define the parameters for showing objects
+params = {
+    'type': 'config',
+    'action': 'get',
+    'xpath': '/config/shared/address',  # Example: to fetch all address objects
+    'key': api_key
+}
+
+url = f"https://{fw_host}/api/"
+
+# Make the API request
+response = requests.get(url, params=params, verify=False)  # Setting verify to False if you want to skip SSL verification
+
+# Parse the XML response
+root = ET.fromstring(response.content)
+
+addresses_from_panorama = {}
+for entry in root.findall(".//entry"):
+    object_name = entry.attrib['name']
+    ip_netmask = entry.find('ip-netmask')
+    if ip_netmask is not None:
+        addresses_from_panorama[object_name] = ip_netmask.text
+
+# Compare
+for name, address in addresses_from_csv.items():
+    if name in addresses_from_panorama:
+        if addresses_from_csv[name] == addresses_from_panorama[name]:
+            print(f"Matched: {name} with address {address}")
+        else:
+            print(f"Name {name} exists but with a different address in Panorama!")
+    else:
+        print(f"Name {name} does not exist in Panorama!")
+
